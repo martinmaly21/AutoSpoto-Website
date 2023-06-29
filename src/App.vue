e51fc5b31e76cf29076c400a9d6a80562f1b60c0
Martin Maly <martinmaly66@hotmail.com>
2023-06-29 12:39:31 +0900

    Remove hello world template code

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MainCard/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainCard from './components/MainCard.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainCard
   }
 }
 </script>
---
+++ src/components/MainCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="main-container">
+        <h1>AutoSpoto</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
