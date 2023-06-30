e06d1c4639c7198f628403dc50553e90a0b54973
Martin Maly <martinmaly66@hotmail.com>
2023-06-30 18:06:52 +0900

    Updated autospoto website to remove rest of template code

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="main-container">
         <h1>AutoSpoto</h1>
+        <h2>Coming July 2023</h2>
     </div>
 </template>
 
@@ -11,5 +12,12 @@ export default {
 </script>
 
 <style scoped>
-
+.main-container {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+     color: #2c3e50;
+     margin-top: 60px;
+}
 </style>
