77e2cb90188710e78b032083d93795216b48fbbc
Andrew <acarava3@uwo.ca>
2023-10-05 08:16:10 -0400

    Slight modifications to os14 dmg

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -1,23 +1,24 @@
 <template>
-    <body>
+  <body>
     <div class="home-page">
-      <div class="top-bar">
+      <header class="top-bar">
         <div class="logo-and-title">
-        <img class="logo" src="@/components/autospoto-app-icon1.png" alt="AutoSpoto">
-        <h1 class="logo-title">AutoSpoto</h1>
-      </div>
-        <div class="tabs">
+          <img class="logo" src="@/components/autospoto-app-icon1.png" alt="AutoSpoto">
+          <h1 class="logo-title">AutoSpoto</h1>
+        </div>
+        <nav class="tabs">
           <div class="tab">About AutoSpoto</div>
           <div class="tab">How It Works</div>
-        </div>
-      </div>
-      <div class="main-container">
+        </nav>
+      </header>
+      <main class="main-container">
         <h2 class="download-heading">Download AutoSpoto</h2>
-        <p><a href="#" class="download-link" @click="downloadDmg">macOS (versions 13.0+)</a></p>
-      </div>
+        <p><a href="#" class="download-link" @click="downloadDmg">macOS 13</a></p>
+        <p><a href="#" class="download-link14" @click="downloadDmg14">macOS 14</a></p>
+      </main>
     </div>
-    </body>
-  </template>
+  </body>
+</template>
 
 <script>
 export default {
@@ -29,30 +30,39 @@ export default {
       downloadLink.setAttribute('download', 'AutoSpoto-Download.dmg');
       downloadLink.click();
     },
+    downloadDmg14() {
+      const dmgFileUrl = 'https://autospoto.xyz/AutoSpoto14.0.dmg';
+      const downloadLink = document.createElement('a');
+      downloadLink.href = dmgFileUrl;
+      downloadLink.setAttribute('download', 'AutoSpoto-Download.dmg');
+      downloadLink.click();
+    },
+
   },
 }
 </script>
 
 <style scoped>
-
-
 *, html, template {
-    margin:0;
-    padding:0;  
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
 
-body{
-    margin:0;
+body {
+    margin: 0;
     padding: 0;
+    font-family: 'JosefinSansRegular', sans-serif;
 }
+
 .top-bar {
-  background-color: #1ed760; /* Spotify green */
-  margin: 0 auto 0;
-  padding: 10px 20px;
+  background-color: #1ed760;
+  padding: 20px;
   display: flex;
   align-items: center;
   justify-content: space-between;
 }
+
 .logo-and-title {
   display: flex;
   align-items: center;
@@ -62,24 +72,21 @@ body{
   max-width: 75px;
   height: auto;
   border-radius: 8px;
-  margin-right: 10px; /* Add margin to separate from the title */
-
+  margin-right: 10px;
 }
 
 .logo-title {
-  font-family: 'JosefinSansRegular', sans-serif;
   font-size: 24px;
   color: rgb(0, 0, 0);
 }
+
 .tabs {
   display: flex;
   gap: 30px;
-  margin-right: 100px;
 }
 
 .tab {
-  font-family: 'JosefinSansRegular', sans-serif;
-  font-size: 24px;
+  font-size: 18px;
   color: rgb(0, 0, 0);
   cursor: pointer;
   transition: color 0.2s ease;
@@ -91,19 +98,16 @@ body{
 
 .main-container {
   text-align: center;
-  color: #1ed760; /* Spotify green */
   padding: 60px 0;
 }
 
 .download-heading {
-  font-family: 'JosefinSansRegular', sans-serif;
   font-size: 24px;
   margin-bottom: 10px;
   color: #1ed760;
 }
 
 .download-link {
-  font-family: 'JosefinSansRegular', sans-serif;
   font-size: 18px;
   color: #1ed760;
   text-decoration: none;
@@ -114,11 +118,20 @@ body{
   color: #149e54;
 }
 
+.download-link14 {
+  font-size: 18px;
+  color: #1ed760;
+  text-decoration: none;
+  transition: color 0.2s ease;
+}
+
+.download-link14:hover {
+  color: #149e54;
+}
+
 .main-container p {
-  font-family: 'JosefinSansRegular', sans-serif;
   font-size: 18px;
   color: #b3b3b3;
   margin-bottom: 20px;
 }
-
 </style>
