0f0d76f54e6a15e7dce80209f114e2bcb2519089
Martin Maly <martinmaly66@hotmail.com>
2024-01-04 08:38:31 +0900

    Added privacy policy

--- src/components/PrivacyCard.vue
+++ src/components/PrivacyCard.vue
@@ -1,6 +1,35 @@
 <template>
 <div class="privacy-card">
+    <div class="privacy-title">
+        Privacy Policy
+    </div>
 
+    <div class="privacy-policy-container">
+        <div class="section">
+            <div class="section-title">User data</div>
+            <div class="section-contents">AutoSpoto does not collect, store or sell any user identifiable data.</div>
+        </div>
+
+        <div class="section">
+            <div class="section-title">Behavioural data</div>
+            <div class="section-contents">AutoSpoto does not collect, store or sell any behavioral data.</div>
+        </div>
+
+        <div class="section">
+            <div class="section-title">Message data</div>
+            <div class="section-contents">AutoSpoto does not collect, store or sell any of your messages. Your messages never leave your device.</div>
+        </div>
+
+        <div class="section">
+            <div class="section-title">Error reporting</div>
+            <div class="section-contents">AutoSpoto uses Firebase for collecting crash data. This may contain device info including OS version, Mac type, and IP Address. The data is only used for fixing AutoSpoto errors. No other analytics are collected.</div>
+        </div>
+
+        <div class="section">
+            <div class="section-title">Licensing</div>
+            <div class="section-contents">AutoSpoto uses Gumroad for checkout and licensing. Please check their privacy policy for data they collect: https://gumroad.com/privacy. AutoSpoto does not handle any payment info.</div>
+        </div>
+    </div>
 </div>
 </template>
 
@@ -11,6 +40,37 @@
     padding: 0;
     scroll-behavior: smooth;
     background: linear-gradient(0deg, rgba(0, 0, 0, 1) 0%, rgba(59, 59, 59, 1) 35%);
+}
+
+
+.privacy-title {
+    text-align: center;
+    font-family: JosefinSansBold;
+    font-size: 20pt;
+    color: white;
+    padding-top: 30px;
+    padding-bottom: 30px;
+}
+
+.privacy-policy-container {
+    margin: auto;
+    max-width: 1200px;
+}
+
+.section {
+    padding-bottom: 20px;
+}
+
+.section-title {
+    font-family: JosefinSansSemiBold;
+    font-size: 18pt;
+    color: white;
+}
+
+.section-contents {
+    font-family: JosefinSansRegular;
+    font-size: 12pt;
+    color: white;
 }
 
 </style>
