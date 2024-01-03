4b102c354726dfd4b1fd19913182a3c668d274d1
Martin Maly <martinmaly66@hotmail.com>
2024-01-04 08:11:55 +0900

    UI finessing on home page

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -183,58 +183,8 @@
     background: linear-gradient(0deg, rgba(0, 0, 0, 1) 0%, rgba(59, 59, 59, 1) 35%);
   }
 
-  .desktop-nav {
-    align-items: flex-start;
-    background-color: transparent;
-    scroll-behavior: smooth;
-    justify-content: flex-end;
-    margin-right: 16px;
-    display: flex;
-    align-items: center;
-  }
-
-  .nav-auto-spoto-logo-container {
-    height: 60px;
-    width: 60px;
-    margin-left: 10px;
-    cursor: pointer;
-    transition: all .2s ease-in-out;
-  }
-  .nav-auto-spoto-logo-container:hover { 
-    transform: scale(1.1);
-  }
-
-  .nav-auto-spoto-logo {
-    width: 100%;
-    height: 100%;
-  }
-
-  .nav-spacer {
-    flex-grow:1 
-  }
-
   hr {
     color: gray;
-  }
-
-  .nav-links {
-    display: flex;
-    transition: all 300ms ease;
-    gap: 2rem;
-    list-style: none;
-    font-size: 1.5rem;
-    scroll-behavior: smooth;
-    color: white;
-    font-family: JosefinSansSemibold;
-    font-size: 16pt;
-  }
-
-  a:hover {
-    color: rgb(56, 210, 17);
-    text-decoration: underline;
-    text-underline-offset: 1rem;
-    text-decoration-color: rgb(56, 210, 17);
-    cursor: pointer;
   }
 
   .main-card {
@@ -244,6 +194,10 @@
     margin-bottom: 20pt;
     margin-left: 20px;
     margin-right: 20px;
+  }
+
+  .auto-spoto-logo-container {
+    margin-top: 15px;
   }
 
   .auto-spoto-logo {
