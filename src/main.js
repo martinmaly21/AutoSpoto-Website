300d6c1d9d64c7d1025b2eba966da76274f80721
Martin Maly <martinmaly66@hotmail.com>
2024-01-05 15:37:14 +0900

    Add anlaytics to web

--- src/main.js
+++ src/main.js
@@ -6,6 +6,27 @@ import MainCard from './components/MainCard.vue'
 import EULACard from './components/EULACard.vue'
 import PrivacyCard from './components/PrivacyCard.vue'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAajOTNZFc-2ZLTtamtVhuJWjSLZ3m2bb0",
+  authDomain: "autospoto-website.firebaseapp.com",
+  projectId: "autospoto-website",
+  storageBucket: "autospoto-website.appspot.com",
+  messagingSenderId: "324990310527",
+  appId: "1:324990310527:web:8c04fc45776b0e4a6c474a",
+  measurementId: "G-NFZTBQ37P7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
 
 createApp(App).mount('#app')
 
