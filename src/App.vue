886e3bbf1712430ea1d223bbc10a5dc17669b0ae
Samuel <samuelcoryn@gmail.com>
2024-09-07 00:04:50 +0200

    :art: Remove newlines

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <template>
     <NavBar />
-
     <RouterView />
-
     <a class="product-hunt" href="https://www.producthunt.com/posts/autospoto?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-autospoto" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=443478&theme=light" alt="AutoSpoto - Never&#0032;lose&#0032;a&#0032;recommended&#0032;song&#0032;again&#0033; | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
     <hr />
-
     <FooterBar/>
   
 </template>
